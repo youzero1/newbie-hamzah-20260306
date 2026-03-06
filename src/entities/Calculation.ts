e9ff32755e8d6c1a8738ad9fe89b0f5404edf0a2
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-06 21:44:26 +0500

    Add src/entities/Calculation.ts

---
+++ src/entities/Calculation.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('calculations')
+export class Calculation {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text' })
+  expression!: string;
+
+  @Column({ type: 'text' })
+  result!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @Column({ type: 'boolean', default: false })
+  isShared!: boolean;
+}
